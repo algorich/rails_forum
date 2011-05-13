35828e92772ea1762b96412b6a6d3ea25a2f614c
Eduardo Hertz <eduardohertz@gmail.com>
2011-05-13 00:08:08 -0300

    Added categories controller routes test.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   scope "forum", :module => "rails_forum", :as => "rails_forum" do
-    resources :categories, :action => "index"
+    resources :categories
     resources :topics, :action => "index"
   end
 
--- spec/routing/category_routing_spec.rb
+++ spec/routing/category_routing_spec.rb
@@ -6,6 +6,26 @@ module RailsForum
       { :get => "/forum/categories" }.should route_to(:controller => "rails_forum/categories", :action => "index")
     end
 
+    it 'should route to #new action' do
+      { :get => "/forum/categories/new" }.should route_to(:controller => "rails_forum/categories", :action => "new")
+    end
+
+    it 'should route to #create action' do
+      { :post => "/forum/categories" }.should route_to(:controller => "rails_forum/categories", :action => "create")
+    end
+
+    it 'should route to #edit action' do
+      { :get => "/forum/categories/5/edit" }.should route_to(:controller => "rails_forum/categories", :action => "edit", :id => "5")
+    end
+
+    it 'should route to #update action' do
+      { :put => "/forum/categories/1" }.should route_to(:controller => "rails_forum/categories", :action => "update", :id => "1")
+    end
+
+    it 'should route to #destroy action' do
+      { :delete => "/forum/categories/1" }.should route_to(:controller => "rails_forum/categories", :action => "destroy", :id => "1")
+    end
+
   end
 end
 
