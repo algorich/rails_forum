97926f7e03993a34718aeacfb1e1a578091adaef
Eduardo Hertz <eduardohertz@gmail.com>
2011-05-13 00:03:08 -0300

    remove useless stuff.

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 module RailsForum
   describe TopicsController do
 
-    it 'should render #index template' do
-      get :index
-      response.should render_template("index")
-    end
-
   end
 end
 
