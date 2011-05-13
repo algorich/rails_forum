ca979b4113d48bf54e28643084bf00bd0214ce89
Eduardo Hertz <eduardohertz@gmail.com>
2011-05-13 00:44:21 -0300

    add controller test for requiring logged in users for some actions.

--- app/controllers/rails_forum/categories_controller.rb
+++ app/controllers/rails_forum/categories_controller.rb
@@ -1,6 +1,8 @@
 module RailsForum
   class CategoriesController < InheritedResources::Base
 
+    before_filter "authenticate_#{RailsForum.devise_model}!", :only => [:new, :edit, :update, :destroy]
+
   end
 end
 
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 module RailsForum
   describe CategoriesController do
 
+    context 'request a logged in subject for' do
+
+      it 'add a new category' do
+        lambda { get :new }.should raise_error(::NoMethodError, "undefined method `authenticate!' for nil:NilClass")
+      end
+
+      it 'edit a category' do
+        lambda { get :edit, :id => "1" }.should raise_error(::NoMethodError, "undefined method `authenticate!' for nil:NilClass")
+      end
+
+      it 'update a category' do
+        lambda { post :update, :id => "2" }.should raise_error(::NoMethodError, "undefined method `authenticate!' for nil:NilClass")
+      end
+
+      it 'destroy a category' do
+        lambda { post :destroy, :id => "2" }.should raise_error(::NoMethodError, "undefined method `authenticate!' for nil:NilClass")
+      end
+
+    end
+
   end
 end
 
